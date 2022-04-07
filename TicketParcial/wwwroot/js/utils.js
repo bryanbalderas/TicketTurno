913c7f856e79b5c6a025deed73774edb399e9d3a
VoN <sudo.von.contact@gmail.com>
2022-04-07 05:01:07 -0500

    Added client side validations

---
+++ TicketParcial/wwwroot/js/utils.js
@@ -0,0 +1,120 @@
+﻿
+/**
+ * @function
+ * @param {any} input - Input that will be validated.
+ * @param {number} minValue - Minimum length allowed.
+ * @returns {boolean} - Returns a boolean depending the validation result.
+ */
+const validateInput = (input, minValue) => {
+    const isValid = input.value.trim().length >= minValue;
+    if (isValid) {
+        input.classList.remove("input-error");
+    }
+    return isValid;
+}
+/**
+ * @function
+ * @param {any} input - Input that will be validated.
+ * @param {number} minValue - Minimum length allowed.
+ * @returns {boolean} - Returns a boolean depending the validation result.
+ */
+const validateSelect = (select) => {
+    const isValid = select.options[select.selectedIndex].value >= 1;
+    if (isValid) {
+        select.classList.remove("input-error");
+    }
+    return isValid;
+}
+/**
+ * @function
+ * @param {any} input - Input that will be validated as a curp.
+ * @returns {boolean} - Returns a boolean depending the validation result.
+ */
+const validateCurp = (input) => {
+
+    if (input.value.trim().length != 18) {
+        return false;
+    };
+
+    const regex = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/;
+    const regexResult = input.value.trim().match(regex);
+
+    if (!regexResult) {
+        return false;
+    }
+
+    input.classList.remove("input-error");
+    return true;
+}
+/**
+ * @function
+ * @param {any} curp - Curp element where we will get the age.
+ * @returns {number} - Returns years difference between date of birth and current year.
+ */
+const getAgeFromCurp = (curp) => {
+
+    const formatedCurp = curp.value.trim().substring(4, 10);
+    let yearCURP = parseInt(formatedCurp.substring(0, 2)) + 1900;
+    const monthCURP = parseInt(formatedCurp.substring(2, 4)) - 1;
+    const dayCURP = parseInt(formatedCurp.substring(4, 6));
+
+    if (yearCURP < 1950) {
+        yearCURP += 100;
+    }
+
+    const studentDate = new Date(yearCURP, monthCURP, dayCURP)
+    const currentDate = Date.now();
+    const differenceDates = Math.abs(currentDate - studentDate);
+    const yearsDifference = parseInt(Math.ceil(differenceDates / (1000 * 60 * 60 * 24)) / 365);
+
+    return yearsDifference;
+}
+
+/**
+ * @function
+ * @param {any} curp - Curp element where we will get the age.
+ * @returns {boolean} - Returns a boolean depending the validation result.
+ */
+const validateAge = (curp) => {
+    let isValid = false;
+    const studentAge = getAgeFromCurp(curp);
+    if (studentAge >= 3 && studentAge <= 15) {
+        isValid = true;
+    }
+    return isValid;
+}
+/**
+ * @function
+ * @param {any} input - Input that will be validated as a curp.
+ * @returns {boolean} - Returns a boolean depending the validation result.
+ */
+const validatePhoneNumber = (input) => {
+    const isValid = input.value.match(/^[0-9]/) && (input.value.length === 10 || input.value.length === 12);
+    if (isValid) {
+        input.classList.remove("input-error");
+    }
+    return isValid;
+}
+/**
+ * @function
+ * @param {any} input - Input that will be validated as a curp.
+ * @returns {boolean} - Returns a boolean depending the validation result.
+ */
+const validateEmail = (input) => {
+    const isValid = input.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+    if (isValid) {
+        input.classList.remove("input-error");
+    }
+    return isValid;
+}
+/**
+ * @function
+ * @param {string} text - Modal's message.
+ */
+const handleError = (text) => {
+    Swal.fire({
+        icon: 'error',
+        title: '¡Ha ocurrido un error!',
+        text,
+    });
+}
---
+++ TicketParcial/wwwroot/js/validations.js
@@ -0,0 +1,134 @@
+﻿const validateTicketForm = (e) => {
+
+    e.preventDefault();
+
+    const processedBy = document.getElementById('nombreRealiza');
+    const curp = document.getElementById('curp');
+    const name = document.getElementById('nombre');
+    const paternalLastName = document.getElementById('paterno');
+    const telephone = document.getElementById('telefono');
+    const cellphone = document.getElementById('celular');
+    const email = document.getElementById('correo');
+    const educationLevel = document.getElementById('nivel');
+    const town = document.getElementById('municipio');
+    const subject = document.getElementById('asunto');
+
+    if (!validateInput(processedBy, 3)) {
+        handleError("Ingresa un nombre válido para continuar.");
+        return false;
+    }
+
+    if (!validateCurp(curp)) {
+        handleError("Ingresa una CURP válida para continuar.");
+        return false;
+    }
+
+    if (!validateAge(curp)) {
+        handleError("Tu fecha de nacimiento no está en el rango permitido de 3 a 15 años para los diferentes tipos de trámite, te recomendamos acudir personalmente a la oficina correspondiente.");
+        return false;
+    }
+
+    if (!validateInput(name, 3)) {
+        handleError("Ingresa un nombre válido para continuar.");
+        return false;
+    }
+
+    if (!validateInput(paternalLastName, 3)) {
+        handleError("Ingresa un apellido paterno válido para continuar.");
+        return false;
+    }
+
+    if (!validatePhoneNumber(telephone, 3)) {
+        handleError("Ingresa un teléfono válido para continuar.");
+        return false;
+    }
+
+    if (!validatePhoneNumber(cellphone)) {
+        handleError("Ingresa un celular válido para continuar.");
+        return false;
+    }
+
+    if (!validateEmail(email)) {
+        handleError("Ingresa un correo válido para continuar.");
+        return false;
+    }
+
+    if (!validateSelect(educationLevel)) {
+        handleError("Selecciona un nivel acádemico para continuar.");
+        return false;
+    }
+
+    if (!validateSelect(town)) {
+        handleError("Selecciona un municipio para continuar.");
+        return false;
+    }
+
+    if (!validateSelect(subject)) {
+        handleError("Selecciona un asunto para continuar.");
+        return false;
+    }
+    
+    document.querySelector("#ticket-form").submit();
+
+};
+
+const validateTownForm = (e) => {
+
+    e.preventDefault();
+
+    const town = document.getElementById('descripcion');
+
+    if (!validateInput(town, 3)) {
+        handleError("Ingresa un municipio válido para continuar.");
+        return false;
+    }
+
+    document.querySelector("#town-form").submit();
+
+};
+
+const validateSubjectForm = (e) => {
+
+    e.preventDefault();
+
+    const subject = document.getElementById('descripcion');
+
+    if (!validateInput(subject, 3)) {
+        handleError("Ingresa un asunto válido para continuar.");
+        return false;
+    }
+
+    document.querySelector("#subject-form").submit();
+
+};
+
+const validateLevelForm = (e) => {
+
+    e.preventDefault();
+
+    const level = document.getElementById('descripcion');
+
+    if (!validateInput(level, 3)) {
+        handleError("Ingresa un nivel válido para continuar.");
+        return false;
+    }
+
+    document.querySelector("#level-form").submit();
+
+};
+
+const confirmDelete = (e) => {
+    e.preventDefault();
+    Swal.fire({
+        title: '¿Estás seguro que quieres proceder?',
+        showConfirmButton: false,
+        showDenyButton: true,
+        showCancelButton: true,
+        denyButtonText: 'Eliminar',
+        cancelButtonText: `Cancelar`,
+    }).then((result) => {
+        if (result.isDenied) {
+            document.querySelector("#form").submit();
+        }
+    })
+}
