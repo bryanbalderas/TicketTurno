5705b1aa749f760da09fc966d8ca663713dbffd0
VoN <sudo.von.contact@gmail.com>
2022-04-07 11:45:37 -0500

    Updated PDF view and controller

--- TicketParcial/Controllers/CrearPDFController.cs
+++ TicketParcial/Controllers/CrearPDFController.cs
@@ -3,35 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Rotativa.AspNetCore;
+using TicketParcial.Infrastructure;
 using TicketParcial.Models;
 
 namespace TicketParcial.Controllers
 {
     public class CrearPDFController : Controller
     {
-       
-        public IActionResult Index() {
-            
-            var ticket = new TicketTurnoModel()
+        private readonly TicketTurnoContext _context;
+
+        public CrearPDFController(TicketTurnoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Asunto/Details/5
+        public async Task<IActionResult> Index(int? id) {
+
+            if (id == null)
             {
-                ID = 1,
-                nombreRealiza = "juanita",
-                curp = "MOBJ790616HCLNSX04",
-                nombre = "hola",
-                paterno = "monjaras",
-                materno = "granados",
-                telefono = "8444103251",
-                celular = "8444103251",
-                correo = "example@example.com"
+                return NotFound();
+            }
 
-            };
+            var ticketModel = await _context.TicketTurnoList
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (ticketModel == null)
+            {
+                return NotFound();
+            }
 
-            return new ViewAsPdf("Index", ticket)
+            return new ViewAsPdf("Index", ticketModel)
             {
                 PageSize = Rotativa.AspNetCore.Options.Size.Letter,
                 PageOrientation = Rotativa.AspNetCore.Options.Orientation.Portrait,
-                //FileName = "Detalle_ticket.pdf"
             };
 
            
